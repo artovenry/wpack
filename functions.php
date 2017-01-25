21ea266b70e7bd8608abb285f07a3613a7f40ba3
vegeta <dev@vegeta.co.jp>
2017-01-25 20:28:27 +0900

    commit

---
+++ functions.php
@@ -0,0 +1,4 @@
+<?
+require __DIR__ . "/vendor/custom_post_type/vendor/autoload.php";
+require __DIR__ . "/lib/renderer.php";
+
--- theme/functions.php
+++
@@ -1,3 +0,0 @@
-<?
-require __DIR__ . "/vendor/autoload.php";
-require __DIR__ . "/lib/renderer.php";
